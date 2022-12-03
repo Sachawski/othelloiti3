aca637d16a3915e9452738b9c9319ccafe6d0bbb
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-03 16:44:39 +0000

    Update TADcoup.h

--- programme/include/TADcoup.h
+++ programme/include/TADcoup.h
@@ -1,6 +1,6 @@
 #ifndef  __TADCOUP__
 #define __TADCOUP__
-#include "definitionTypes.h""
+#include "definitionTypes.h"
 
 Coup coupCoup(Pion pion,Position pos);
 Pion coupObtenirPionCoup(Coup coup);
