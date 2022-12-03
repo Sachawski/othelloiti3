848793d5834a879bf10a85ef97124de1909ce1e7
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-03 16:42:14 +0000

    Update TADcouleur.h

--- programme/include/TADcouleur.h
+++ programme/include/TADcouleur.h
@@ -1,4 +1,13 @@
 #ifndef  __TADCOULEUR__
 #define __TADCOULEUR__
 
+struct Couleur {
+  typedef enum [couleur] {Blanc,Noir}
+}
+
+Couleur noir();
+Couleur blanc();
+int estBlanc(Couleur c);
+Couleur changerCouleur(Couleur c1);
+
 #endif
