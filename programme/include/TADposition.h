46796a76a533a1486c6875f793dcd14899e85371
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-03 17:28:33 +0000

    Update TADposition.h

--- programme/include/TADposition.h
+++ programme/include/TADposition.h
@@ -1,4 +1,9 @@
 #ifndef  __TADPOSITION__
 #define __TADPOSITION__
+#include "definitionTypes.h"
+
+Position position([1..8] largeur, hauteur );
+[1..8] obtenirX(Position unePosition);
+[1..8] obtenirY(Position unePosition);
 
 #endif
