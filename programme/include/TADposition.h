135894a3250c7a74a126558915d833b1e0878db3
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-03 17:32:01 +0000

    Commentaire ensemble d'entiers TADposition.h

--- programme/include/TADposition.h
+++ programme/include/TADposition.h
@@ -2,10 +2,10 @@
 #define __TADPOSITION__
 #include "definitionTypes.h"
 
-Position position([1..8] largeur, hauteur );
-[1..8] obtenirX(Position unePosition);
-[1..8] obtenirY(Position unePosition);
+Position position(int t[8] largeur, hauteur );
+int t[8] obtenirX(Position unePosition);
+int t[8] obtenirY(Position unePosition);
 
 #endif
 
-/* Je sais plus comment on définit proprement un ensemble d'entiers, c'est juste int t[8] ? */
+/* Je sais plus comment on définit proprement un ensemble d'entiers, c'est juste int t[8] en déclarant t avant ? */
