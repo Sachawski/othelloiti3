2b91d79008bca3c8fca0ad8e9e23d47f600f6faa
vturnel <vturnel@insa-rouen.fr>
2022-12-04 11:39:08 +0100

    Ajout du développement du TAD coup (tests + def + implémentation)

--- programme/include/TADcoup.h
+++ programme/include/TADcoup.h
@@ -1,9 +1,54 @@
-#ifndef  __TADCOUP__
-#define __TADCOUP__
-#include "definitionTypes.h"
+/**
+2 * \file TADcoup.h
+3 * \brief Implantation du TAD Coup
+4 * \author V. Turnel
+5 * \version 1.0
+6 * \date 03/12/2022
+7 *
+8 */
 
-Coup coupCoup(Pion pion,Position pos);
-Pion coupObtenirPionCoup(Coup coup);
-Position coupObtenirPositionCoup(Coup coup);
+#ifndef __TADcoup__
+#define __TADcoup__
+#include "TADpion.h"
+#include "TADposition.h"
+
+typedef struct
+{
+    /* Modélisation d'un coup par ses deux caractéristiques :
+        - pion qui est joué
+        - position où le pion est placé
+    */
+
+   PN_Pion pion;
+   POS_Position position;
+
+} CP_Coup;
+
+/**
+* \fn P_Coup CP_coup(PN_Pion pion, POS_Position position)
+* \brief Création d’un Coup à partir d'un pion et d'une position
+* \param pion pièce concernée par le coup
+* \param position emplacement du coup
+* \return CP_Coup
+*/
+CP_Coup CP_coup(PN_Pion pion, POS_Position position);
+
+/**
+* \fn PN_Pion CP_pion(CP_Coup coup)
+* \brief Obtention du pion concerné par le coup
+* \param coup le coup
+* \return PN_Pion
+*/
+PN_Pion CP_pion(CP_Coup coup);
+
+/**
+* \fn POS_Position CP_position(CP_Coup coup)
+* \brief Obtention de la position du coup
+* \param coup le coup
+* \return POS_Position
+*/
+POS_Position CP_position(CP_Coup coup);
 
 #endif
+
+
---
+++ programme/src/TADcoup.c
@@ -0,0 +1,16 @@
+#include "../include/TADcoup.h"
+
+CP_Coup CP_coup(PN_Pion pion, POS_Position position) {
+    CP_Coup cp;
+
+    cp.pion = pion;
+    cp.position = position;
+}
+
+PN_Pion CP_pion(CP_Coup coup) {
+    return coup.pion;
+}
+
+POS_Position CP_position(CP_Coup coup) {
+    return coup.position;
+}
