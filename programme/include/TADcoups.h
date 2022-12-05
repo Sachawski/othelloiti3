f35e19eb3899021da0c718a2b19c3e59deb7b9c1
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-05 21:57:47 +0000

    Implantation TADcoups.h

--- programme/include/TADcoups.h
+++ programme/include/TADcoups.h
@@ -1,4 +1,13 @@
-#ifndef  __TADCOUPS__
+#ifndef __TADCOUPS__
 #define __TADCOUPS__
+#include "definitionTypes.h"
+
+CPS_Coups CPS_coups();
+
+int CPS_nbCoups(CPS_Coups cps);
+
+void CPS_ajouterCoups(CPS_Coups cps, CP_Coup unCoup);
+
+CP_Coup CPS_iemeCoup(CPS_Coups cps, unsigned short int place);
 
 #endif
