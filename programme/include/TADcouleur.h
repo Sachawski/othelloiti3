ee0af9e7d54d429b31b5077c5cb9871cd5ee3359
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-06 09:40:51 +0000

    Update TADcouleur.h

--- programme/include/TADcouleur.h
+++ programme/include/TADcouleur.h
@@ -9,25 +9,30 @@
 
 #ifndef  __TADCOULEUR__
 #define __TADCOULEUR__
-#include "definitionTypes.h"
 
 
- /* Modélisation de la couleur Noir
+/* Modélisation d'une couleur qui est soit noir, soit blanche
+    */
+typedef enum {BLANC,NOIR} CLR_Couleur ;
+
+
+
+ /* Modélisation de la couleur NOIR
     */
 CLR_Couleur CLR_noir();
 
- /* Modélisation de la couleur Blanc
+ /* Modélisation de la couleur BLANC
     */
 CLR_Couleur CLR_blanc();
 
- /* fonction permettant de savoir si la couleur est Blanc.
-    Retourne Vrai (1) si la couleur est Blanc et Faux (0) si la couleur est Noir
+ /* fonction permettant de savoir si la couleur est BLANC.
+    Retourne Vrai (1) si la couleur est BLANC et Faux (0) si la couleur est NOIR
     */
 int CLR_estBlanc(CLR_Couleur c);
 
  /* fonction qui change la couleur d'une couleur donnée
-    Si la couleur donnée est Blanc, la fonction changerCouleur retourne Noir
-    Si la couleur donnée est Noir, la fonction changerCouleur retourne Blanc
+    Si la couleur donnée est BLANC, la fonction changerCouleur retourne NOIR
+    Si la couleur donnée est NOIR, la fonction changerCouleur retourne BLANC
     */
 
 CLR_Couleur CLR_changerCouleur(CLR_Couleur c1);
