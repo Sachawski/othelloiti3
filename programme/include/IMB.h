b6806e954e28dba3205c24a2bb9dd5e07fb5c186
swojciechowski <sacha.wojciechowski@insa-rouen.fr>
2022-12-29 23:55:51 +0100

    ajout de la doc dans IMB.h

--- programme/include/IMB.h
+++ programme/include/IMB.h
@@ -3,8 +3,23 @@
 
 #define LONGUEUR_COUP 3
 
+/**
+* \fn CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur)
+* \brief Fonction qui permet de lire le coup envoyer par le broker
+* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels
+* \param couleur Couleur du broker
+* \param profondeur Inutilisé ici, présente pour des raisons de cohérence au niveau des appels
+* \return CP_Coup (Coup du broker)
+*/
 CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur) ;
 
+/**
+* \fn void envoiBroker(PLT_Plateau plateau, CP_Coup coup)
+* \brief Procédure qui permet d'écrire le coup de l'IA dans le terminal pour le transmettre au broker
+* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels
+* \param coup Coup joué par l'IA
+* \return void
+*/
 void envoiBroker(PLT_Plateau plateau, CP_Coup coup);
 
 #endif
