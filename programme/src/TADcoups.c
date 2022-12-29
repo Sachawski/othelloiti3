ec8486e54b4b1a5c65fac8e52055598d36420449
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-29 15:22:15 +0000

    Ajout des commentaires de TADcoups.c

--- programme/src/TADcoups.c
+++ programme/src/TADcoups.c
@@ -6,6 +6,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * \fn CPS_Coups CPS_coups()
+ * \brief Fonction qui retourne une liste de Coups vide
+ * \return CPS_Coups
+ */
+
 CPS_Coups CPS_coups() {
   CPS_Coups resultat;
 
@@ -13,14 +20,34 @@ CPS_Coups CPS_coups() {
   return resultat;
 }
 
+
+/**
+ * \fn int CPS_nbCoups(CPS_Coups cps)
+ * \brief Fonction qui retourne le nombre de Coups d'une liste de Coups
+ * \return int
+ */
+
 int CPS_nbCoups(CPS_Coups cps){
   return cps.nbTotalCoups;
 }
+
+/**
+ * \fn CPS_ajouterCoups(CPS_Coups *cps, CP_Coup unCoup)
+ * \brief Ajoute un Coup dans une liste de Coups
+ * \param CPS_Coups
+ * \param CP_Coup
+ */
 
 void CPS_ajouterCoups(CPS_Coups *cps, CP_Coup unCoup){
   cps->nbTotalCoups=cps->nbTotalCoups+1;
   cps->lesCoups[cps->nbTotalCoups]=unCoup;
 }
+
+/**
+ * \fn CP_Coup CPS_iemeCoup(CPS_Coups cps, unsigned short int place)
+ * \brief Fonction qui retourne le Coup à la place i d'une liste de Coups
+ * \return CP_Coup
+ */
 
 CP_Coup CPS_iemeCoup(CPS_Coups cps, unsigned short int place){
   return cps.lesCoups[place];
