b4bc2957c67fe88a32bae84c062872e2817fc5e5
Thulliez Paul <paul.thulliez@insa-rouen.fr>
2022-12-29 15:32:30 +0000

    Ajout des commentaires de TADcouleur.c

--- programme/src/TADcouleur.c
+++ programme/src/TADcouleur.c
@@ -1,15 +1,42 @@
 #include <stdlib.h>
 #include "TADcouleur.h"
 
+/**
+ * \fn CLR_Couleur CLR_noir()
+ * \brief Fonction qui retourne la couleur NOIR
+ * \return CLR_Couleur
+ */
+
 CLR_Couleur CLR_noir(){
 	return NOIR;
 }
+
+/**
+ * \fn CLR_Couleur CLR_blanc()
+ * \brief Fonction qui retourne la couleur BLANC
+ * \return CLR_Couleur
+ */
+
 CLR_Couleur CLR_blanc(){
 	return BLANC;
 }
+
+/**
+ * \fn int  CLR_estBlanc(CLR_Couleur c)
+ * \brief Fonction qui retourne un booleen. Si la couleur est BLANC alors le booleen retourne Vrai, sinon il retourne Faux.
+ * \return bool
+ */
+
 int CLR_estBlanc(CLR_Couleur c){
 	return c==BLANC;
 }
+
+/**
+ * \fn CLR_Couleur CLR_changerCouleur(CLR_Couleur c1)
+ * \brief Fonction qui retourne la couleur adverse à celle donnée en entrée.
+ * \return CLR_Couleur
+ */
+
 CLR_Couleur CLR_changerCouleur(CLR_Couleur c1){
 	if(CLR_estBlanc(c1)){
 		return NOIR;
