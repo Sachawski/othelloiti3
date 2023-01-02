a3a6b0cef96a020046602bd81f83fdaa312547d1
vturnel <vturnel@insa-rouen.fr>
2023-01-02 09:18:11 +0100

    Ajout documentation .h

--- programme/include/IHM.h
+++ programme/include/IHM.h
@@ -1,3 +1,12 @@
+/**
+2 * \file IHM.h
+3 * \brief Implantation de la fonction de l'IHM
+4 * \author V. Turnel, S. Wojciechowski
+5 * \version 1.0
+6 * \date 04/12/2022
+7 *
+8 */
+
 #ifndef __IHM__
 #define __IHM__
 
@@ -10,12 +19,21 @@
 #include "TADpion.h"
 
 /**
-* \fn afficherPlateau(PLT_Plateau plateau)
+* \fn afficherPlateau(PLT_Plateau plateau, CP_Coup coup, int possibilite)
 * \brief Affiche le plateau actuel dans le terminal.
 * \param plateau Représente le plateau de jeu.
+* \param coup Inutile de cette procédure.
+* \param possibilite Indique si le joueur à la possibilité de jouer.
 */
 void afficherPlateau(PLT_Plateau plateau, CP_Coup coup, int possibilite);
 
+
+/**
+* \fn afficherFinDePartie(CLR_Couleur couleurGagnante, EtatPartie etat)
+* \brief Affiche L'état de fin de partie dans le terminal.
+* \param couleurGagnante La couleur gagnante en cas de victoire.
+* \param etat Etat du plateau de la partie en fin de jeu : Victoire | Egalite.
+*/
 void afficherFinDePartie(CLR_Couleur couleurGagnante, EtatPartie etat);
 
 /**
--- programme/include/IMB.h
+++ programme/include/IMB.h
@@ -1,3 +1,12 @@
+/**
+2 * \file IMB.h
+3 * \brief Implantation de la fonction de l'IMB
+4 * \author V. Turnel, S. Wojciechowski
+5 * \version 1.0
+6 * \date 04/12/2022
+7 *
+8 */
+
 #ifndef __IMB__
 #define __IMB__
 
@@ -5,10 +14,10 @@
 
 /**
 * \fn CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur)
-* \brief Fonction qui permet de lire le coup envoyer par le broker
-* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels
-* \param couleur Couleur du broker
-* \param profondeur Inutilisé ici, présente pour des raisons de cohérence au niveau des appels
+* \brief Fonction qui permet de lire le coup envoyer par le broker.
+* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels.
+* \param couleur Couleur du broker.
+* \param profondeur Inutilisé ici, présente pour des raisons de cohérence au niveau des appels.
 * \return CP_Coup (Coup du broker)
 */
 CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur) ;
@@ -18,10 +27,15 @@ CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profonde
 * \brief Procédure qui permet d'écrire le coup de l'IA dans le terminal pour le transmettre au broker
 * \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels
 * \param coup Coup joué par l'IA
-* \return void
 */
 void envoiBroker(PLT_Plateau plateau, CP_Coup coup, int possibilite);
 
+/**
+* \fn finDePartieBroker(CLR_Couleur couleurGagnante, EtatPartie etat)
+* \brief Indique l'état de fin de partie en mode tournois.
+* \param couleurGagnante La couleur gagnante en cas de victoire.
+* \param etat Etat du plateau de la partie en fin de jeu : Victoire | Egalite.
+*/
 void finDePartieBroker(CLR_Couleur couleurGagnante, EtatPartie etat);
 
 #endif
