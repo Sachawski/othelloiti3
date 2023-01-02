5be81d4d580442e7b5d2825d785f5c9f884b39be
vturnel <vturnel@insa-rouen.fr>
2023-01-02 09:17:10 +0100

    Ajout documentation

--- programme/src/IMB.c
+++ programme/src/IMB.c
@@ -1,3 +1,12 @@
+/**
+2 * \file IHM.c
+3 * \brief Fichier contenant les fonctions de l'IMB
+4 * \author V. Turnel, S. Wojciechowski
+5 * \version 1.0
+6 * \date 04/12/2022
+7 *
+8 */
+
 #include "TADposition.h"
 #include "TADpion.h"
 #include "TADcouleur.h"
@@ -12,6 +21,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/**
+* \fn CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur)
+* \brief Fonction qui permet de lire le coup envoyer par le broker.
+* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels.
+* \param couleur Couleur du broker.
+* \param profondeur Inutilisé ici, présente pour des raisons de cohérence au niveau des appels.
+* \return CP_Coup (Coup du broker)
+*/
 CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profondeur) {
     CP_Coup coupBroker ;
     PN_Pion pionBroker ;
@@ -33,6 +51,12 @@ CP_Coup interactionBroker(PLT_Plateau plateau, CLR_Couleur couleur, int profonde
     return coupBroker ;
 }
 
+/**
+* \fn void envoiBroker(PLT_Plateau plateau, CP_Coup coup)
+* \brief Procédure qui permet d'écrire le coup de l'IA dans le terminal pour le transmettre au broker
+* \param plateau Inutilisé ici, présente pour des raisons de cohérence au niveaud des appels
+* \param coup Coup joué par l'IA
+*/
 void envoiBroker(PLT_Plateau plateau, CP_Coup coup, int possibilite) {
     int x,y ;
     char xChar,yChar;
@@ -62,6 +86,12 @@ void envoiBroker(PLT_Plateau plateau, CP_Coup coup, int possibilite) {
 
 }
 
+/**
+* \fn finDePartieBroker(CLR_Couleur couleurGagnante, EtatPartie etat)
+* \brief Indique l'état de fin de partie en mode tournois.
+* \param couleurGagnante La couleur gagnante en cas de victoire.
+* \param etat Etat du plateau de la partie en fin de jeu : Victoire | Egalite.
+*/
 void finDePartieBroker(CLR_Couleur couleurGagnante, EtatPartie etat){
   if(etat==partieGagnee){
     if(couleurGagnante == NOIR)
